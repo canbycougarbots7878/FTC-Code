1e90c83cad270f15919aa468c7d25542275d5023
Cougarbots7878 <canbycougar7878@gmail.com>
2024-09-19 16:26:53 -0700

    added AutoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name = "Auto Test", group = "Concept")
+public class AutoTest extends LinearOpMode {
+
+    private DcMotor motorFR = null;
+    private DcMotor motorFL = null;
+    private DcMotor motorBR = null;
+    private DcMotor motorBL = null;
+
+    @Override
+    public void runOpMode() {
+        motorFL = hardwareMap.get(DcMotor.class, "FrontLeft");
+        motorBL = hardwareMap.get(DcMotor.class, "BackLeft");
+        motorFR = hardwareMap.get(DcMotor.class, "FrontRight");
+        motorBR = hardwareMap.get(DcMotor.class, "BackRight");
+
+        motorFR.setDirection(DcMotor.Direction.FORWARD);
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            motorFR.setPower(1);
+            motorFL.setPower(-1);
+            motorBR.setPower(1);
+            motorBL.setPower(-1);
+        }
+    }
+}
