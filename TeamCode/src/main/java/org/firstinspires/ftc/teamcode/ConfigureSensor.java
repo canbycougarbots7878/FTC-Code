86be44e0f311a0f88a585777894fb048973d937f
canbycougarbots7878 <cougarbots7878@gmail.com>
2024-10-08 17:26:45 -0700

    added Color Sensing Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConfigureSensor.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+@Config
+public class ConfigureSensor extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ColorRangefinder crf =
+                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color"));
+
+        /*
+        Using this example configuration, you can detect all three sample colors based on which pin is reading true:
+        both      --> yellow
+        only pin0 --> red
+        only pin1 --> blue
+        neither   --> no object
+         */
+        crf.setPin0Digital(ColorRangefinder.DigitalMode.HSV, 180 / 360.0 * 255, 250 / 360.0 * 255); // blue
+        crf.setPin0Digital(ColorRangefinder.DigitalMode.HSV, 55 / 360.0 * 255, 75 / 360.0 * 255); // yellow
+        crf.setPin0DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 20); // 20mm or closer requirement
+
+        crf.setPin1Digital(ColorRangefinder.DigitalMode.HSV, 140 / 360.0 * 255, 210 / 360.0 * 255); // inverted red
+        crf.setPin1Digital(ColorRangefinder.DigitalMode.HSV, 235 / 360.0 * 255, 255 / 360.0 * 255); // inverted yellow
+        crf.setPin1DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 20); // 20mm or closer requirement
+        crf.setPin1InvertHue(); // invert hue values
+
+        waitForStart();
+
+        stop();
+    }
+}
