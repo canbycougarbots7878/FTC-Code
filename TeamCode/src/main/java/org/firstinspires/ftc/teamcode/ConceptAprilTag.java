17263e3c2bcf4bbaae0b5d4b77b1982c65cff829
Cougarbots7878 <canbycougar7878@gmail.com>
2024-10-24 17:22:20 -0700

    Auto-updated so android studio would be happy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTag.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
-
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
@@ -14,179 +15,143 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-@TeleOp(name = "Concept: AprilTag", group = "Concept")
-
+@TeleOp(name="Omni Drive To AprilTag", group = "Concept")
 public class ConceptAprilTag extends LinearOpMode {
-
-    private AprilTagProcessor aprilTag;
-    private VisionPortal visionPortal;
-
-    private DcMotor motorFR      = null;
-    private DcMotor motorFL      = null;
-    private DcMotor motorBR      = null;
-    private DcMotor motorBL      = null;
-
-    final double SPEED_GAIN      = 0.02;
-    final double STRAFE_GAIN     = 0.015;
-    final double TURN_GAIN       = 0.01;
-    final double MAX_AUTO_SPEED  = 0.5;
-    final double MAX_AUTO_STRAFE = 0.5;
-    final double MAX_AUTO_TURN   = 0.3;
     final double DESIRED_DISTANCE = 12.0;
+    final double SPEED_GAIN  =  0.02 ;
+    final double STRAFE_GAIN =  0.015;
+    final double TURN_GAIN   =  0.01 ;
 
-    @Override
+    final double MAX_AUTO_SPEED = 0.5;
+    final double MAX_AUTO_STRAFE= 0.5;
+    final double MAX_AUTO_TURN  = 0.3;
 
-    public void runOpMode() {
+    private DcMotor leftFrontDrive  = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive   = null;
+    private DcMotor rightBackDrive  = null;
+
+    private static final int DESIRED_TAG_ID = -1;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private AprilTagDetection desiredTag = null;
+
+    @Override public void runOpMode() {
+        boolean targetFound = false;
+        double  drive       = 0;
+        double  strafe      = 0;
+        double  turn        = 0;
         initAprilTag();
-        initMotors();
-        setManualExposure();
-
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "FrontLeft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "FrontRight");
+        leftBackDrive  = hardwareMap.get(DcMotor.class,  "BackLeft");
+        rightBackDrive = hardwareMap.get(DcMotor.class,  "BackRight");
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        setManualExposure(6, 250);  // Use low exposure time to reduce motion blur
+        telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
         telemetry.addData(">", "Touch Play to start OpMode");
         telemetry.update();
-
-        double drive;
-        double strafe;
-        double turn;
-
         waitForStart();
-
-        if (opModeIsActive()) {
-
-            while (opModeIsActive()) {
-
-                List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-
-                if (!currentDetections.isEmpty()) {
-
-                    AprilTagDetection desiredTag = currentDetections.get(0);
-                    //telemetryAprilTag(currentDetections);
-
-                    if (desiredTag.ftcPose.range > DESIRED_DISTANCE){
-
-                        driveRobot(desiredTag.ftcPose.range - DESIRED_DISTANCE);
-
+        while (opModeIsActive()) {
+            targetFound = false;
+            desiredTag  = null;
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                if (detection.metadata != null) {
+                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;
+                    } else {
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
                     }
-                    else if (desiredTag.ftcPose.range < DESIRED_DISTANCE) {
-
-                        driveRobot(desiredTag.ftcPose.range - DESIRED_DISTANCE);
-
-                    }
-                    else
-
-                        stopRobot();
-
-                    double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-                    double  headingError    = desiredTag.ftcPose.bearing;
-                    double  yawError        = desiredTag.ftcPose.yaw;
-
-                    sleep(10);
-                }
-                else {
-                    stopRobot();
+                } else {
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
                 }
             }
+
+            if (targetFound) {
+                telemetry.addData("\n>","HOLD Left-Bumper to Drive to Target\n");
+                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+                telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+                telemetry.addData("Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
+            } else {
+                telemetry.addData("\n>","Drive using joysticks to find valid target\n");
+            }
+            if (targetFound) {
+                double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                double  headingError    = desiredTag.ftcPose.bearing;
+                double  yawError        = desiredTag.ftcPose.yaw;
+                drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+                strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+            } else {
+                drive  = -gamepad1.left_stick_y  / 2.0;
+                strafe = -gamepad1.left_stick_x  / 2.0;
+                turn   = -gamepad1.right_stick_x / 3.0;
+                telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+            }
+            telemetry.update();
+            moveRobot(drive, strafe, turn);
+            sleep(10);
         }
-        visionPortal.close();
     }
-    private void initAprilTag() {
-
-        aprilTag = new AprilTagProcessor.Builder().build();
-        aprilTag.setDecimation(2);
-
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        builder.addProcessor(aprilTag);
-
-        visionPortal = builder.build();
-    }
-    private void initMotors() {
-        motorFL = hardwareMap.get(DcMotor.class, "FrontRight");
-        motorBL = hardwareMap.get(DcMotor.class, "BackLeft");
-        motorFR = hardwareMap.get(DcMotor.class, "FrontLeft");
-        motorBR = hardwareMap.get(DcMotor.class, "BackRight");
-
-        motorFR.setDirection(DcMotor.Direction.FORWARD);
-        motorFL.setDirection(DcMotor.Direction.REVERSE);
-        motorBR.setDirection(DcMotor.Direction.FORWARD);
-        motorBL.setDirection(DcMotor.Direction.REVERSE);
-    }
-
-    public void setMotors(double rightFrontPower, double leftFrontPower, double rightBackPower, double leftBackPower) {
-
-        motorFL.setPower(leftFrontPower);
-        motorFR.setPower(rightFrontPower);
-        motorBL.setPower(-leftBackPower);
-        motorBR.setPower(-rightBackPower);
-
-    }
-    public void driveRobot(double x) {
-
-        double leftFrontPower    =  x;
-        double rightFrontPower   =  x;
-        double leftBackPower     =  x;
-        double rightBackPower    =  x;
+    public void moveRobot(double x, double y, double yaw) {
+        double leftFrontPower    =  x -y +yaw;
+        double rightFrontPower   =  x +y -yaw;
+        double leftBackPower     =  x +y +yaw;
+        double rightBackPower    =  x -y -yaw;
         double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
-
         if (max > 1.0) {
-
             leftFrontPower /= max;
             rightFrontPower /= max;
             leftBackPower /= max;
             rightBackPower /= max;
-
         }
-
-        setMotors(rightFrontPower,leftFrontPower,rightBackPower,leftBackPower);
-
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
     }
-    private void setManualExposure() {
-
+    private void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(2);
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+    private void    setManualExposure(int exposureMS, int gain) {
         if (visionPortal == null) {
-
             return;
         }
         if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
             telemetry.addData("Camera", "Waiting");
             telemetry.update();
-
             while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
-
                 sleep(20);
-
             }
-
             telemetry.addData("Camera", "Ready");
             telemetry.update();
-
         }
         if (!isStopRequested()) {
-
             ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-
             if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
-
                 exposureControl.setMode(ExposureControl.Mode.Manual);
                 sleep(50);
-
             }
-
-            exposureControl.setExposure(6, TimeUnit.MILLISECONDS);
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
             sleep(20);
-
             GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-            gainControl.setGain(250);
+            gainControl.setGain(gain);
             sleep(20);
-
         }
     }
-
-    private void stopRobot() {
-        setMotors(0,0,0,0);
-    }
-
 }
