a24b1d6c9858744f24c4f0738bfa21f21505b0fe
canbycougarbots7878 <cougarbots7878@gmail.com>
2024-11-19 17:10:27 -0800

    autonomous - FR is negative on test robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -17,8 +17,8 @@ public class auto extends LinearOpMode {
     DcMotor motorBL = null;
     DcMotor motorFR = null;
     DcMotor motorBR = null;
-    DcMotor arm1 = null;
-    DcMotor arm2 = null;
+    //DcMotor arm1 = null;
+    //DcMotor arm2 = null;
 
     public void runOpMode() {
         motorFL = hardwareMap.get(DcMotor.class, "FrontLeft");
@@ -26,27 +26,52 @@ public class auto extends LinearOpMode {
         motorFR = hardwareMap.get(DcMotor.class, "FrontRight");
         motorBR = hardwareMap.get(DcMotor.class, "BackRight");
 
-        arm1 = hardwareMap.get(DcMotor.class, "Arm1");
-        arm2 = hardwareMap.get(DcMotor.class, "Arm2");
-        CRServo clawRotation = hardwareMap.get(CRServo.class, "clawRotation");
-        CRServo claw = hardwareMap.get(CRServo.class, "claw");
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+       // arm1 = hardwareMap.get(DcMotor.class, "Arm1");
+       // arm2 = hardwareMap.get(DcMotor.class, "Arm2");
+       // CRServo clawRotation = hardwareMap.get(CRServo.class, "clawRotation");
+        //CRServo claw = hardwareMap.get(CRServo.class, "claw");
 
         motorFR.setDirection(DcMotor.Direction.FORWARD);
         motorFL.setDirection(DcMotor.Direction.REVERSE);
         motorBR.setDirection(DcMotor.Direction.FORWARD);
         motorBL.setDirection(DcMotor.Direction.REVERSE);
 
-        arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        arm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //arm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        while (opModeIsActive()) {
-            Forward(1,0.5);
-            Turn(1,0.5);
+        TelemetryPosition();
+        Forward(1000, 0.5);
+        while (motorFL.isBusy() && motorBL.isBusy() && motorBR.isBusy() && motorFR.isBusy()){
+
+        }
+        TelemetryPosition();
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+        //12.762720155208534in forward?
+        while (opModeIsActive()){
+
         }
 
     }
@@ -66,13 +91,17 @@ public class auto extends LinearOpMode {
         motorBL.setPower(Power);
         motorFR.setPower(Power);
         motorBR.setPower(Power);
+
     }
 
     private void Forward(int TD, double Power) {
-        motorFL.setTargetPosition(TD*1440);
-        motorBL.setTargetPosition(TD*1440);
-        motorFR.setTargetPosition(TD*1440);
-        motorBR.setTargetPosition(TD*1440);
+        double C = 104 * 3.14159;
+        double CountsPerMillimeter = 1440/C;
+        int Millimeter = (int)(CountsPerMillimeter);
+        motorFL.setTargetPosition(TD * Millimeter);
+        motorBL.setTargetPosition(TD * Millimeter);
+        motorFR.setTargetPosition(TD * Millimeter);
+        motorBR.setTargetPosition(TD * Millimeter);
 
         motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -84,5 +113,14 @@ public class auto extends LinearOpMode {
         motorFR.setPower(Power);
         motorBR.setPower(Power);
 
+    }
+
+
+    private void TelemetryPosition() {
+        telemetry.addData("Front Right", motorFR.getCurrentPosition());
+        telemetry.addData("Front Left", motorFL.getCurrentPosition());
+        telemetry.addData("Back Right", motorBR.getCurrentPosition());
+        telemetry.addData("Back Left", motorBL.getCurrentPosition());
+        telemetry.update();
     }
 }
