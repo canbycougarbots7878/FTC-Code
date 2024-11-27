2a3179ddd95020cecefc735ebe6e140855a2ded9
Scott Baker <scott@perturb.org>
2024-11-27 15:36:34 -0800

    Normalize the motor init for consistency with auto.java (Clockwise)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionRobotCode.java
@@ -12,20 +12,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "COMPETITION: Manual Drive", group = "Competition")
 public class CompetitionRobotCode extends LinearOpMode {
     public void runOpMode() {
-        DcMotor motorFL = hardwareMap.get(DcMotor.class, "FrontLeft");
-        DcMotor motorBL = hardwareMap.get(DcMotor.class, "BackLeft");
+		// Init all the motors
         DcMotor motorFR = hardwareMap.get(DcMotor.class, "FrontRight");
         DcMotor motorBR = hardwareMap.get(DcMotor.class, "BackRight");
+        DcMotor motorBL = hardwareMap.get(DcMotor.class, "BackLeft");
+        DcMotor motorFL = hardwareMap.get(DcMotor.class, "FrontLeft");
 
         DcMotor arm1 = hardwareMap.get(DcMotor.class, "Arm1");
         Servo arm2 = hardwareMap.get(Servo.class, "Arm2");
         //CRServo clawRotation = hardwareMap.get(CRServo.class, "clawRotation");
         CRServo claw = hardwareMap.get(CRServo.class, "claw");
 
+		// Set each motor's direction
         motorFR.setDirection(DcMotor.Direction.FORWARD);
-        motorFL.setDirection(DcMotor.Direction.REVERSE);
         motorBR.setDirection(DcMotor.Direction.FORWARD);
         motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
 
         arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -34,30 +36,34 @@ public class CompetitionRobotCode extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            double axial = -gamepad1.left_stick_y;
+            double axial   = -gamepad1.left_stick_y;
             double lateral = gamepad1.left_stick_x;
-            double yaw = gamepad1.right_stick_x;
-            double speed = .5;
+            double yaw     = gamepad1.right_stick_x;
+            double speed   = .5;
+
+			// Setup quick speed modifications for precision driving
             if (gamepad1.right_bumper) {
                 speed = 1;
             } else if (gamepad1.left_bumper) {
                 speed = .25;
             }
 
-            double leftFrontPower = axial + lateral - yaw;
             double rightFrontPower = axial - lateral + yaw;
-            double leftBackPower = axial - lateral - yaw;
-            double rightBackPower = axial + lateral + yaw;
+            double rightBackPower  = axial + lateral + yaw;
+            double leftBackPower   = axial - lateral - yaw;
+            double leftFrontPower  = axial + lateral - yaw;
 
             motorFR.setPower(speed * leftFrontPower);
-            motorFL.setPower(speed * rightFrontPower);
             motorBR.setPower(speed * leftBackPower);
             motorBL.setPower(speed * rightBackPower);
+            motorFL.setPower(speed * rightFrontPower);
+
             double Arm2pos = 1 - gamepad1.right_trigger;
             telemetry.addData("Arm1 Position", arm1.getCurrentPosition());
             arm2.setPosition(Arm2pos);
             telemetry.addData("Arm2 Position", Arm2pos);
             String mode = "Home";
+
             if (gamepad1.a) { mode = "Home"; }
             if (gamepad1.b) { mode = "Deploy"; }
             if (gamepad1.x) { mode = "Basket"; }
