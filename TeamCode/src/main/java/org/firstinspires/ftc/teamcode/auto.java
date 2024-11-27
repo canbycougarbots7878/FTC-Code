9ed87b1bafa1be73f2e13c128b041a4465737c78
Scott Baker <scott@perturb.org>
2024-11-26 17:21:05 -0800

    Spell out variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -63,14 +63,15 @@ public class auto extends LinearOpMode {
         }
     }
 
-    private void Turn(int TR, double Power) {
-        double Circumference = 104 * Math.PI;
+    private void Turn(int TargetRadius, double Power) {
+        double Circumference       = 104 * Math.PI;
         double CountsPerMillimeter = 1440/Circumference;
-        int Millimeter = (int)(CountsPerMillimeter);
-        motorFR.setTargetPosition(TR * Millimeter);
-        motorBR.setTargetPosition(TR * Millimeter);
-        motorBL.setTargetPosition(-TR * Millimeter);
-        motorFL.setTargetPosition(-TR * Millimeter);
+        int Millimeter             = (int)(CountsPerMillimeter);
+
+        motorFR.setTargetPosition(TargetRadius  * Millimeter);
+        motorBR.setTargetPosition(TargetRadius  * Millimeter);
+        motorBL.setTargetPosition(-TargetRadius * Millimeter);
+        motorFL.setTargetPosition(-TargetRadius * Millimeter);
 
         motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
