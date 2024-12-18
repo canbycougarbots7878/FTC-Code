df9cf2048f105b454c7a41bcae87971c3a20e156
Roland_Baker <canbycougar7878@gmail.com>
2024-12-17 17:26:38 -0800

    CompetitionTeleopV2.java is in perfect condition to deliver a microphone to a person of high importance.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleopV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleopV2.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.MovementLib.*;
 @TeleOp(name = "Competition TeleOp", group = "Competition")
@@ -13,11 +14,17 @@ public class CompetitionTeleopV2 extends LinearOpMode {
     @Override
     public void runOpMode() {
         // Initialize Motors
-        DcMotor Front_Right = hardwareMap.get(DcMotor.class, "Front Right");
-        DcMotor Front_Left = hardwareMap.get(DcMotor.class, "Front Left");
-        DcMotor Back_Right = hardwareMap.get(DcMotor.class, "Back Right");
-        DcMotor Back_Left = hardwareMap.get(DcMotor.class, "Back Left");
+        DcMotor Front_Right = hardwareMap.get(DcMotor.class, "FrontRight");
+        DcMotor Front_Left = hardwareMap.get(DcMotor.class, "FrontLeft");
+        DcMotor Back_Right = hardwareMap.get(DcMotor.class, "BackRight");
+        DcMotor Back_Left = hardwareMap.get(DcMotor.class, "BackLeft");
+        Front_Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Front_Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Back_Right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Back_Left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         DriveWheels Wheels = new DriveWheels(Front_Right, Front_Left, Back_Right, Back_Left);
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Right.setDirection(DcMotorSimple.Direction.REVERSE);
         // Initialize Arm
         //Arm = hardwareMap.get(DcMotor.class, "Extending Arm");
         Slider = hardwareMap.get(DcMotor.class, "Slide Arm");
@@ -26,17 +33,37 @@ public class CompetitionTeleopV2 extends LinearOpMode {
         Slider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         Slider.setPower(0);
         // Initialize Claw
-        //Claw = hardwareMap.get(Servo.class, "Claw");
+        Claw = hardwareMap.get(Servo.class, "Claw");
         //Wrist = hardwareMap.get(Servo.class, "Wrist");
         // Variables
         boolean debounce = true;
+        boolean extend = false;
         waitForStart();
         while(opModeIsActive()) {
             Wheels.Omni_Move(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, 1);
-            if(gamepad1.right_bumper && debounce) {
-
+            if (gamepad1.dpad_up) {
+                if(debounce) {
+                    debounce = false;
+                    extend = !extend;
+                }
             }
-            //what ag
+            else {
+                debounce = true;
+            }
+            if (extend) {
+                Slider.setTargetPosition(4500);
+                Slider.setPower(1);
+            }
+            else {
+                Slider.setTargetPosition(0);
+                if (Slider.getCurrentPosition() < 100) {
+                    Slider.setPower(0);
+                }
+                else {
+                    Slider.setPower(-.5);
+                }
+            }
+            Claw.setPosition(1 - gamepad1.right_trigger);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementLib.java
@@ -19,15 +19,12 @@ public class MovementLib {
             this.Back_Right = Back_Right;
             //pneumonoultramicroscopicsilicovolcanoconiosis
             this.Back_Left = Back_Left;
-            Front_Right.setDirection(DcMotor.Direction.REVERSE);
-            Front_Left.setDirection(DcMotor.Direction.REVERSE);
-            Back_Left.setDirection(DcMotor.Direction.REVERSE);
         }
         public void Omni_Move(double Forward, double Right, double Rotate, double speed) {
-            double Front_Left_Power = - Forward - Right - Rotate;
-            double Front_Right_Power = - Forward + Right + Rotate;
-            double Back_Right_Power = - Forward - Right + Rotate;
-            double Back_Left_Power = - Forward + Right - Rotate;
+            double Front_Left_Power = - Forward + Right + Rotate;
+            double Front_Right_Power = - Forward - Right - Rotate;
+            double Back_Right_Power = - Forward + Right - Rotate;
+            double Back_Left_Power = - Forward - Right + Rotate;
             this.Front_Right.setPower(Front_Right_Power);
             this.Front_Left.setPower(Front_Left_Power);
             this.Back_Right.setPower(Back_Right_Power);
