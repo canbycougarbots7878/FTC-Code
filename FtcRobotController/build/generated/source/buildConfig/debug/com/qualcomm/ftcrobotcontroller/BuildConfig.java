dc4c429cfe357be1c954be4f3d4c00de84c140f3
Roland_Baker <canbycougar7878@gmail.com>
2024-12-26 16:31:31 -0800

    Creating PRM vs RRM program for driver Wesley

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-12-19T17:27:07.195-0800";
+  public static final String APP_BUILD_TIME = "2024-12-26T16:22:08.283-0800";
 }
