d216c791e8132f2b16a614367263d683c69bfc4f
canbycougarbots7878 <cougarbots7878@gmail.com>
2025-04-15 17:28:35 -0700

    commit OTOS :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
@@ -1,238 +1,171 @@
-//1S
+/*
+    SPDX-License-Identifier: MIT
 
+    Copyright (c) 2024 SparkFun Electronics
+*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-//Notes
-// 1. 90 degrees counterclockwise is 450 mm
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Disabled
-@Autonomous(name = "Auto Drive Test", group = "Concept")
+/*
+ * This OpMode illustrates how to use the SparkFun Qwiic Optical Tracking Odometry Sensor (OTOS)
+ *
+ * The OpMode assumes that the sensor is configured with a name of "sensor_otos".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ *
+ * See the sensor's product page: https://www.sparkfun.com/products/24904
+ */
+@TeleOp(name = "autoTest", group = "Sensor")
 public class autoTest extends LinearOpMode {
-    double Tau = Math.PI * 2;
-    DcMotor motorFL = null;
-    DcMotor motorBL = null;
-    DcMotor motorFR = null;
-    DcMotor motorBR = null;
+    // Create an instance of the sensor
+    SparkFunOTOS myOtos;
 
-    DcMotor arm      = null;
-    DcMotor slidearm = null;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Get a reference to the sensor
+        DcMotor Front_Right = hardwareMap.get(DcMotor.class, "FrontRight");
+        DcMotor Front_Left = hardwareMap.get(DcMotor.class, "FrontLeft");
+        DcMotor Back_Right = hardwareMap.get(DcMotor.class, "BackRight");
+        DcMotor Back_Left = hardwareMap.get(DcMotor.class, "BackLeft");
+        MovementLib.DriveWheels Wheels = new MovementLib.DriveWheels(Front_Right, Front_Left, Back_Right, Back_Left);
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Left.setDirection(DcMotorSimple.Direction.REVERSE);
+        myOtos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
+        double loops = 0;
 
-    Servo claw  = null;
-    Servo wrist = null;
-    public void runOpMode() {
-        motorFR = hardwareMap.get(DcMotor.class, "FrontRight");
-        motorBR = hardwareMap.get(DcMotor.class, "BackRight");
-        motorBL = hardwareMap.get(DcMotor.class, "BackLeft");
-        motorFL = hardwareMap.get(DcMotor.class, "FrontLeft");
+        // All the configuration for the OTOS is done in this helper method, check it out!
+        // This also calibrates the sensor
+        configureOtos();
 
-        //slidearm = hardwareMap.get(DcMotor.class, "Slide Arm");
-        //arm = hardwareMap.get(DcMotor.class, "Extending Arm");
-
-        //wrist = hardwareMap.get(Servo.class, "wrist");
-        //claw = hardwareMap.get(Servo.class, "claw");
-
-        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        motorFR.setDirection(DcMotor.Direction.REVERSE);
-        motorBR.setDirection(DcMotor.Direction.REVERSE);
-        motorBL.setDirection(DcMotor.Direction.FORWARD);
-        motorFL.setDirection(DcMotor.Direction.FORWARD);
-
-        //slidearm.setTargetPosition(100);
-        //slidearm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //slidearm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //slidearm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        //arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        // Wait for the start button to be pressed
         waitForStart();
 
-        // Print starting numbers and move forward
-        TelemetryPosition();
-        int rest = 5000;
-        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //Home();
-
-        Forward(1000, 0.75);
-
-        while (motorFL.isBusy() || motorBL.isBusy() || motorBR.isBusy() || motorFR.isBusy()) {
-            TelemetryPosition();
-        }
-
-        // Print ending numbers
-        TelemetryPosition();
-
-        motorFR.setPower(0);
-        motorBR.setPower(0);
-        motorBL.setPower(0);
-        motorFL.setPower(0);
-
-        // Leave the data on the screen so we have time to write it down
+        // Loop until the OpMode ends
         while (opModeIsActive()) {
-            // Do nothing, just wait
+            // Get the latest position, which includes the x and y coordinates, plus the
+            // heading angle
+            SparkFunOTOS.Pose2D pos = myOtos.getPosition();
+
+            // Inform user of available controls
+            telemetry.addLine("Press Y (triangle) on Gamepad to reset tracking");
+            telemetry.addLine("Press X (square) on Gamepad to calibrate the IMU");
+            telemetry.addLine();
+
+            // Log the position to the telemetry
+            telemetry.addData("X coordinate", pos.x);
+            telemetry.addData("Y coordinate", pos.y);
+            telemetry.addData("Heading angle", pos.h);
+
+            // Update the telemetry on the driver station
+            telemetry.update();
+
+            if (pos.y < 2){
+                Wheels.Omni_Move(1,0,0,0.50);
+            }
+            if (pos.h < -0.01){
+                Wheels.Omni_Move(0,0,1,0.25);
+            } else if (pos.h > 0.01) {
+                Wheels.Omni_Move(0,0,-1,0.25);
+            }
+            if (pos.x < -0.01){
+                Wheels.Omni_Move(0,1,0,0.25);
+            } else if (pos.x > 0.01) {
+                Wheels.Omni_Move(0,-1,0,0.25);
+            }
+            Wheels.Omni_Move(0,0,0,0);
         }
     }
 
-    /*
-    private void SetSliderPosition(int target) {
-        slidearm.setTargetPosition(target);
-        slidearm.setPower(.5);
-    }
+    private void configureOtos() {
+        telemetry.addLine("Configuring OTOS...");
+        telemetry.update();
 
-    private void SetSliderSegment(int segment) {
-        //slidearm.setPower(1);
-        switch(segment) {
-            case(0): SetSliderPosition(100); break;
-            case(1): SetSliderPosition(1437); break;
-            case(2): SetSliderPosition(2971); break;
-            case(3): SetSliderPosition(4498); break;
-            case(4): SetSliderPosition(6025); break;
-        }
-    }
+        // Set the desired units for linear and angular measurements. Can be either
+        // meters or inches for linear, and radians or degrees for angular. If not
+        // set, the default is inches and degrees. Note that this setting is not
+        // persisted in the sensor, so you need to set at the start of all your
+        // OpModes if using the non-default value.
+        myOtos.setLinearUnit(DistanceUnit.METER);
+        // myOtos.setLinearUnit(DistanceUnit.INCH);
+        // myOtos.setAngularUnit(AnguleUnit.RADIANS);
+        myOtos.setAngularUnit(AngleUnit.DEGREES);
 
-    private void SetArmPosition(int position) {
-        boolean direction = (arm.getCurrentPosition() < position);
-        int proximity = Math.abs(arm.getCurrentPosition() - position);
-        double speed = ProximityRunge(proximity);
-        if(direction) {
-            arm.setPower(speed);
-        }
-        else {
-            arm.setPower(-speed);
-        }
-    }
+        // Assuming you've mounted your sensor to a robot and it's not centered,
+        // you can specify the offset for the sensor relative to the center of the
+        // robot. The units default to inches and degrees, but if you want to use
+        // different units, specify them before setting the offset! Note that as of
+        // firmware version 1.0, these values will be lost after a power cycle, so
+        // you will need to set them each time you power up the sensor. For example, if
+        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
+        // forward (positive Y) of the center of the robot, and mounted 90 degrees
+        // clockwise (negative rotation) from the robot's orientation, the offset
+        // would be {-5, 10, -90}. These can be any value, even the angle can be
+        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        myOtos.setOffset(offset);
 
-    private double ProximityRunge(int position) {
-        double x = (double)position / 4;
-        return 1 - 1/(1+x*x);
-    }
+        // Here we can set the linear and angular scalars, which can compensate for
+        // scaling issues with the sensor measurements. Note that as of firmware
+        // version 1.0, these values will be lost after a power cycle, so you will
+        // need to set them each time you power up the sensor. They can be any value
+        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
+        // first set both scalars to 1.0, then calibrate the angular scalar, then
+        // the linear scalar. To calibrate the angular scalar, spin the robot by
+        // multiple rotations (eg. 10) to get a precise error, then set the scalar
+        // to the inverse of the error. Remember that the angle wraps from -180 to
+        // 180 degrees, so for example, if after 10 rotations counterclockwise
+        // (positive rotation), the sensor reports -15 degrees, the required scalar
+        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
+        // robot a known distance and measure the error; do this multiple times at
+        // multiple speeds to get an average, then set the linear scalar to the
+        // inverse of the error. For example, if you move the robot 100 inches and
+        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
+        myOtos.setLinearScalar(1.0);
+        myOtos.setAngularScalar(1.0);
 
-     */
+        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
+        // have an offset. Note that as of firmware version 1.0, the calibration
+        // will be lost after a power cycle; the OTOS performs a quick calibration
+        // when it powers up, but it is recommended to perform a more thorough
+        // calibration at the start of all your OpModes. Note that the sensor must
+        // be completely stationary and flat during calibration! When calling
+        // calibrateImu(), you can specify the number of samples to take and whether
+        // to wait until the calibration is complete. If no parameters are provided,
+        // it will take 255 samples and wait until done; each sample takes about
+        // 2.4ms, so about 612ms total
+        myOtos.calibrateImu();
 
-    private void Forward(double TargetDistance, double Power) {
-        //motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Reset the tracking algorithm - this resets the position to the origin,
+        // but can also be used to recover from some rare tracking errors
+        myOtos.resetTracking();
 
-        double Circumference = 52 * Tau;
-        double CountsPerMillimeter = 1440 / Circumference;
+        // After resetting the tracking, the OTOS will report that the robot is at
+        // the origin. If your robot does not start at the origin, or you have
+        // another source of location information (eg. vision odometry), you can set
+        // the OTOS location to match and it will continue to track from there.
+        SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
+        myOtos.setPosition(currentPosition);
 
-        motorFR.setTargetPosition((int) (TargetDistance * CountsPerMillimeter));
-        motorBR.setTargetPosition((int) (TargetDistance * CountsPerMillimeter));
-        motorBL.setTargetPosition((int) (TargetDistance * CountsPerMillimeter));
-        motorFL.setTargetPosition((int) (TargetDistance * CountsPerMillimeter));
+        // Get the hardware and firmware version
+        SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
+        SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
+        myOtos.getVersionInfo(hwVersion, fwVersion);
 
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        //I am going to do acceleration for the wheels
-
-        motorFR.setPower(Power);
-        motorBR.setPower(Power);
-        motorBL.setPower(Power);
-        motorFL.setPower(Power);
-
-        // Wait until the motors are done spinning
-
-    }
-
-    private void TelemetryPosition() {
-        telemetry.addData("Front Right", motorFR.getCurrentPosition());
-        telemetry.addData("Back Right", motorBR.getCurrentPosition());
-        telemetry.addData("Back Left", motorBL.getCurrentPosition());
-        telemetry.addData("Front Left", motorFL.getCurrentPosition());
+        telemetry.addLine("OTOS configured! Press start to get position data!");
+        telemetry.addLine();
+        telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
+        telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
         telemetry.update();
     }
-
-    private void Telemetry(int Word) {
-        if (Word == 1) {
-            telemetry.addData("", "Arm");
-            telemetry.update();
-        } else if (Word == 2) {
-            telemetry.addData("", "Claw");
-            telemetry.update();
-        } else if (Word == 3) {
-            telemetry.addData("", "Lifter");
-            telemetry.update();
-        }
-    }
-
-    private void Turn(double TargetRadians, double Power) {
-        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        double Circumference = 52 * Tau;
-        double CountsPerMillimeter = 1440 / Circumference;
-
-        motorFR.setTargetPosition((int) (TargetRadians * CountsPerMillimeter));
-        motorBR.setTargetPosition((int) (TargetRadians * CountsPerMillimeter));
-        motorBL.setTargetPosition((int) (-TargetRadians * CountsPerMillimeter));
-        motorFL.setTargetPosition((int) (-TargetRadians * CountsPerMillimeter));
-
-        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        motorFR.setPower(Power);
-        motorBR.setPower(Power);
-        motorBL.setPower(Power);
-        motorFL.setPower(Power);
-
-        // Wait until the motors are done spinning
-        while (motorFL.isBusy() && motorBL.isBusy() && motorBR.isBusy() && motorFR.isBusy()) {
-            // Do nothing, just wait
-        }
-
-    }
-
-    /*
-    private void Home() {
-        SetSliderSegment(0);
-        SetArmPosition(0);
-    }
-
-    private void Deploy() {
-        SetSliderSegment(0);
-        SetArmPosition(-70);
-    }
-
-    private void Basket() {
-        SetSliderSegment(1);
-    }
-
-    private void Reach() {
-        SetSliderSegment(2);
-    }
-
-     */
-
-
 }
-
