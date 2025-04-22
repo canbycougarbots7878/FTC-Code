5c46503eda43a3c82724b50d6bce04e81989d9a3
Roland_Baker <canbycougar7878@gmail.com>
2025-04-22 16:29:10 -0700

    OTOS test 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
@@ -74,10 +74,10 @@ public class autoTest extends LinearOpMode {
                 double forward = 0;
                 double h_offset = 0;
                 if(pos.h < -5) { // To far right
-                    h_offset = .1;
+                    h_offset = .5;
                 }
                 else if(pos.h > 5) {
-                    h_offset = -.1;
+                    h_offset = -.5;
                 }
                 else {
                     h_offset = 0;
